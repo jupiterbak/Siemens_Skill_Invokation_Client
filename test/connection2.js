7f4ce598648592c98f852aa437bd3f34be57eb3d
jupiterbak <jupiterbak2002@yahoo.fr>
2020-12-07 14:20:55 +0100

    Example of tests mit puppeter

--- test/connection2.js
+++ test/connection2.js
@@ -7,33 +7,35 @@ var expect  = chai.expect;
 var should = chai.should();
 var chaiHttp = require("chai-http");
 chai.use(chaiHttp);
+const puppeteer = require('puppeteer');
 
 var request = require('request');
 const { fail } = require("yargs");
 var server = require("../app/backend/index.js");
 
 
-describe ("Connections", function(){
-    this.timeout(5000);
+describe ("Connections 2", function(){
+    this.timeout(500000);
     describe ("TEST Swagger API", function() {
-        // it("Settings", done=>{
-        //     request("http://localhost:7400/api/v1/skills" , function(error, response, body) {
-        //         expect(response.statusCode).to.equal(200);
-        //         done();
-        //     });
-        // });
-
-        it("Simple", done=>{
-            assert.equal(2+3, 5);
-            done();
+        
+        it("Settings", done=>{
+            request("http://localhost:7400/api/v1/skills" , function(error, response, body) {
+                expect(response.statusCode).to.equal(200);
+                done();
+            });
         });
     
-        // it("Variables", done=>{
-        //     request("http://localhost:7400/api/v1/skills" , function(error, response, body) {
-        //         expect(response.statusCode).to.equal(200);
-        //         done();
-        //     });
-        // });
+        it("Circuit sreenshot", done=>{            
+
+            (async () => {
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+                await page.goto('https://example.com/');
+                await page.screenshot({path: 'C:\GitHub\Siemens_Skill_Invokation_Client_Restrukturierung\test\results/circuit.png'});
+                await browser.close();
+                done();
+            })();
+        });
 
         // it("Variables2", done=>{
         //     request("http://localhost:7400/api/v1/skills" , function(error, response, body) {
